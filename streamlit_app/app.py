be3e8cc644295768015af4f5e6417e8fa3299226
Erik Valasek <valasek.ekonomika@gmail.com>
2026-06-13 18:26:36 +0200

    feat: add top rated movies table

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 from services.analytics import get_kpis
+from services.analytics import get_top_rated
 
 st.set_page_config(
     page_title="Movie Analytics",
@@ -9,6 +10,8 @@ st.set_page_config(
 
 st.title("🎬 Movie Analytics Dashboard")
 
+
+top_movies = get_top_rated()
 kpis = get_kpis()
 
 col1, col2 = st.columns(2)
@@ -22,3 +25,7 @@ col2.metric(
     "Average Rating",
     round(kpis["avg_rating"], 2)
 )
+
+
+st.subheader("⭐ Top Rated Movies")
+st.dataframe(top_movies)
