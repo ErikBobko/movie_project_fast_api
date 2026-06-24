ae8c1c2febfd1c4b2fe1e2453b6e187704bab5d3
Erik Valasek <valasek.ekonomika@gmail.com>
2026-06-24 20:21:23 +0200

    refactor: move API requests into api client service

---
+++ services/api_client.py
@@ -0,0 +1,46 @@
+
+API_URL = "http://127.0.0.1:8000"
+import requests
+
+
+def get_actor(tmdb_actor_id: int):
+    try:
+        response = requests.get(f"{API_URL}/actors/{tmdb_actor_id}", timeout=5)
+        response.raise_for_status()
+        return response.json()
+    except requests.RequestException:
+        return None
+
+
+def get_movie_crew(tmdb_id: int):
+    try:
+        response = requests.get(f"{API_URL}/movies/{tmdb_id}/crew", timeout=5)
+        response.raise_for_status()
+        return response.json()
+    except requests.RequestException:
+        return {
+            "directors": [],
+            "writers": [],
+            "composers": []
+        }
+
+def get_movie_cast(tmdb_id: int):
+    response = requests.get(f"{API_URL}/movies/{tmdb_id}/cast")
+    response.raise_for_status()
+    return response.json()
+
+def get_actor_movies(tmdb_actor_id: int):
+    try:
+        response = requests.get(f"{API_URL}/actors/{tmdb_actor_id}/movies", timeout=5)
+        response.raise_for_status()
+        return response.json()
+    except requests.RequestException:
+        return []
+
+def get_movie_by_id(movie_id: int):
+    try:
+        response = requests.get(f"{API_URL}/movies/by-id/{movie_id}", timeout=5)
+        response.raise_for_status()
+        return response.json()
+    except requests.RequestException:
+        return None
